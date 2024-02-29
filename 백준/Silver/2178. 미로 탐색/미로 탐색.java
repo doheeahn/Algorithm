40f48e548013e93c964e3a2e07f0fab01bca2913
doheeahn <123529618+doheeahn@users.noreply.github.com>
2024-02-29 23:05:16 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 14636 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -6,68 +6,60 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main{
+	static int[] di = {-1,0,1,0};
+	static int[] dj = {0,1,0,-1};
+	
+	public static class Index {
+		int i,j;
+		
+		public Index(int i, int j) {
+			this.i = i;
+			this.j = j;
+		}
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[][] arr = new int[n][m];
+		
+		for(int i=0;i<n;i++) {
+			String s = br.readLine();
+			for(int j=0;j<m;j++) {
+				arr[i][j] = s.charAt(j)-'0';
+			}
+		}
+		
+		Queue<Index> queue = new LinkedList<>();
+		queue.offer(new Index(0,0));
+		int ans=1;
+		
+		while(!queue.isEmpty()) {
+			int num = queue.size();
+			ans++;
+			
+			for(int i=0;i<num;i++) {
+				Index cur = queue.poll();
+				for(int d=0;d<4;d++) {
+					int x = cur.i+di[d];
+					int y = cur.j+dj[d];
 
-    static int n,m;
-    static int[] di= {-1,0,1,0};
-    static int[] dj = {0,1,0,-1};
-    static Queue<Node> queue = new LinkedList<>();
-    static char[][] map;
-
-    public static class Node{
-        int i,j;
-
-        public Node(int i, int j) {
-            this.i = i;
-            this.j = j;
-        }
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n= Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new char[n][m];
-
-        for(int i=0;i<n;i++){
-            String s = br.readLine();
-            map[i] = s.toCharArray();
-        }
-
-        queue.offer(new Node(0,0));
-        map[0][0]='2';
-        int ans = bfs(1);
-        System.out.println(ans);
-
-
-
-    }
-
-    private static int bfs(int cnt) {
-
-        while(!queue.isEmpty()){
-            int num = queue.size();
-
-            for(int t=0;t<num;t++){
-                Node cur = queue.poll();
-                int i = cur.i;
-                int j = cur.j;
-                if(i+1==n && j+1==m){
-                    queue.clear();
-                    break;
-                }
-                for(int d=0;d<4;d++){
-
-                    if(i+di[d]>=0 && i+di[d]<n && j+dj[d]>=0 && j+dj[d]<m && map[i+di[d]][j+dj[d]]=='1'){
-                        map[i+di[d]][j+dj[d]]='2';
-                        queue.offer(new Node(i+di[d],j+dj[d]));
-                    }
-                }
-            }
-            cnt++;
-        }
-
-        return cnt-1;
-    }
+					if(x>=0 && x<n && y>=0 && y<m && arr[x][y]==1) {
+						if(x==n-1 && y==m-1) {
+							System.out.println(ans);
+							return;
+						}
+						arr[x][y]=2;
+						queue.offer(new Index(x, y));
+					}
+				}
+			}
+			
+		}
+		
+		
+	}
 
 }
